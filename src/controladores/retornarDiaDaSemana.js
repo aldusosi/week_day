775845b86857645a81728b305d00db397a198439
Alexsandro Duarte <80373334+aldusosi@users.noreply.github.com>
2021-09-01 14:43:06 -0300

    trabalhando validações em startDay e amountOfDays

--- src/controladores/retornarDiaDaSemana.js
+++ src/controladores/retornarDiaDaSemana.js
@@ -2,13 +2,18 @@ const VerificarDia = require("../utilities/verificaDiaDaSemana");
 
 function retornarDiaDaSemana(req, res){
     const diaAtual = String(req.query.startDay);
-    const quantDiasASePassar = Number(req.query.amountOfDays);
-    
-    console.log(diaAtual)
+    const quantDiasASePassar = Math.trunc(Number(req.query.amountOfDays));
+
+    if(diaAtual != 'domingo' && diaAtual != 'segunda' && diaAtual != 'terca' && diaAtual != 'quarta' && diaAtual != 'quinda'
+    && diaAtual != 'sexta' && diaAtual != 'sabado' ){
+        return res.status(400).json('Utilize os seguintes formatos, domingo, segunda, terca, quarta, quinta, sexta, sabado');
+    }
+
+    if(quantDiasASePassar != Number(quantDiasASePassar) || quantDiasASePassar < 0){
+        return res.status(400).json('amountOfDays precisa ser do tipo Number e maior que zero.')
+    }
 
     res.json(VerificarDia(diaAtual, quantDiasASePassar));
-
-
 }
 
 module.exports = retornarDiaDaSemana;
